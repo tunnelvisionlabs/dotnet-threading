b9df42ad1f45e17a8510a5dc42454f39d1febfef
Sam Harwell <sam.harwell@rackspace.com>
2014-07-25 12:35:34 -0500

    Add summary documentation for the Rackspace.Threading namespace

---
+++ Rackspace.Threading/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿namespace Rackspace.Threading
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The <see cref="Rackspace.Threading"/> namespace contains classes for simplifying the
+    /// development of asynchronous code which supports .NET 3.5 and newer, without relying
+    /// on <see langword="async/await"/>.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
