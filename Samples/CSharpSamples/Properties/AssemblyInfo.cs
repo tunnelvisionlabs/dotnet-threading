13966d0485d2df9ff1fa8acdd0788517369234ec
Sam Harwell <sam.harwell@rackspace.com>
2014-07-25 11:10:27 -0500

    Add C# sample project

---
+++ Samples/CSharpSamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Rackspace, US Inc. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpSamples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rackspace Inc.")]
+[assembly: AssemblyProduct("CSharpSamples")]
+[assembly: AssemblyCopyright("Copyright © Sam Harwell 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+#if !PORTABLE
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0-dev")]
