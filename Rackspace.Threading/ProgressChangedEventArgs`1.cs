e52e822ec27ac01404a128c9fdec95111d34b57b
Sam Harwell <sam.harwell@rackspace.com>
2014-07-28 11:31:08 -0500

    Add the Progress<T> default implementation of IProgress<T>

---
+++ Rackspace.Threading/ProgressChangedEventArgs`1.cs
@@ -0,0 +1,50 @@
+﻿namespace Rackspace.Threading
+{
+    using System;
+
+    /// <summary>
+    /// This class contains information for the <see cref="Progress{T}.ProgressChanged"/> event.
+    /// </summary>
+    /// <remarks>
+    /// Prior to .NET 4.5, the <see cref="EventHandler{TEventArgs}"/> delegate included a
+    /// constraint that the generic argument be derived from <see cref="EventArgs"/>. This
+    /// constraint prevents a direct back-port of the <see cref="T:System.Progress{T}"/>
+    /// class to earlier frameworks, so the <see cref="Progress{T}"/> class is provided with
+    /// a wrapper around the progress value reported by the
+    /// <see cref="Progress{T}.ProgressChanged"/> event.
+    /// </remarks>
+    /// <typeparam name="T">The type representing the progress indicator.</typeparam>
+    /// <threadsafety static="true" instance="false"/>
+    /// <preliminary/>
+    public class ProgressChangedEventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// This is the backing field for the <see cref="Progress"/> property.
+        /// </summary>
+        private readonly T _progress;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressChangedEventArgs{T}"/> class
+        /// with the specified progress.
+        /// </summary>
+        /// <param name="progress">An instance of <typeparamref name="T"/> representing the current progress.</param>
+        public ProgressChangedEventArgs(T progress)
+        {
+            _progress = progress;
+        }
+
+        /// <summary>
+        /// Gets the current progress.
+        /// </summary>
+        /// <value>
+        /// An instance of <typeparamref name="T"/> representing the current progress.
+        /// </value>
+        public T Progress
+        {
+            get
+            {
+                return _progress;
+            }
+        }
+    }
+}
---
+++ Rackspace.Threading/Progress`1.cs
@@ -0,0 +1,91 @@
+﻿namespace Rackspace.Threading
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// Provides an <see cref="IProgress{T}"/> that invokes callbacks for each reported progress value.
+    /// </summary>
+    /// <remarks>
+    /// Any handler provided to the constructor or event handlers registered with the
+    /// <see cref="ProgressChanged"/> event are invoked through a <see cref="SynchronizationContext"/>
+    /// instance captured when the instance is constructed. If there is no current
+    /// <see cref="SynchronizationContext"/> at the time of construction, the callbacks will be invoked
+    /// on the <see cref="T:System.Threading.ThreadPool"/>.
+    /// </remarks>
+    /// <typeparam name="T">Specifies the type of the progress report value.</typeparam>
+    public class Progress<T> : IProgress<T>
+    {
+        private readonly SynchronizationContext _synchronizationContext;
+        private readonly SendOrPostCallback _callback;
+        private readonly Action<T> _handler;
+
+        /// <summary>
+        /// Raised for each reported progress value.
+        /// </summary>
+        /// <remarks>
+        /// Handlers registered with this event will be invoked on the <see cref="SynchronizationContext"/>
+        /// captured when the instance was constructed.
+        /// </remarks>
+        public event EventHandler<ProgressChangedEventArgs<T>> ProgressChanged;
+
+        /// <summary>
+        /// Initializes the <see cref="Progress{T}"/> object.
+        /// </summary>
+        public Progress()
+        {
+            _synchronizationContext = SynchronizationContext.Current;
+            _callback = Callback;
+        }
+
+        /// <summary>
+        /// Initializes the <see cref="Progress{T}"/> object with the specified callback.
+        /// </summary>
+        /// <param name="handler">
+        /// A handler to invoke for each reported progress value. This handler will be invoked in addition
+        /// to any delegates registered with the <see cref="ProgressChanged"/> event. Depending on the
+        /// <see cref="SynchronizationContext"/> instance captured by the <see cref="Progress{T}"/> at
+        /// construction, it is possible that this handler instance could be invoked concurrently with
+        /// itself.
+        /// </param>
+        public Progress(Action<T> handler)
+            : this()
+        {
+            _handler = handler;
+        }
+
+        /// <inheritdoc/>
+        /// <remarks>
+        /// Any handler provided to the constructor or event handlers registered with the
+        /// <see cref="ProgressChanged"/> event are invoked through a <see cref="SynchronizationContext"/>
+        /// instance captured when the instance is constructed. If there is no current
+        /// <see cref="SynchronizationContext"/> at the time of construction, the callbacks will be invoked
+        /// on the <see cref="T:System.Threading.ThreadPool"/>.
+        /// </remarks>
+        void IProgress<T>.Report(T value)
+        {
+            OnReport(value);
+        }
+
+        /// <summary>
+        /// Reports a progress change.
+        /// </summary>
+        /// <param name="value">The value of the updated progress.</param>
+        protected virtual void OnReport(T value)
+        {
+            if (_handler != null || ProgressChanged != null)
+                _synchronizationContext.Post(_callback, value);
+        }
+
+        private void Callback(object state)
+        {
+            T value = (T)state;
+            if (_handler != null)
+                _handler(value);
+
+            EventHandler<ProgressChangedEventArgs<T>> t = ProgressChanged;
+            if (t != null)
+                t(this, new ProgressChangedEventArgs<T>(value));
+        }
+    }
+}
