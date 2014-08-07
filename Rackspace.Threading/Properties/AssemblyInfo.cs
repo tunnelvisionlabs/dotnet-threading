551f3b28a51649f09e5051e9e91876029e7ef67c
Sam Harwell <sam@tunnelvisionlabs.com>
2014-08-07 15:57:00 -0500

    Prepare for development of release 1.1

--- Rackspace.Threading/Properties/AssemblyInfo.cs
+++ Rackspace.Threading/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 #endif
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1.0-dev")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.0-dev")]
