b2babdd0de7463fd1d743de55c3cd82da12a61cb
Sam Harwell <sam@tunnelvisionlabs.com>
2014-08-09 16:38:10 -0500

    Updated assembly and build info for release 1.1.0-beta001

--- Rackspace.Threading/Properties/AssemblyInfo.cs
+++ Rackspace.Threading/Properties/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0.0-dev")]
+[assembly: AssemblyInformationalVersion("1.1.0.0-beta001")]
