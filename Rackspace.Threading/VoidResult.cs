891f7ca72446f4bebe8760662290a16a8e3fc089
Sam Harwell <sam@tunnelvisionlabs.com>
2015-03-26 18:20:35 -0500

    Add documentation for VoidResult

--- Rackspace.Threading/VoidResult.cs
+++ Rackspace.Threading/VoidResult.cs
@@ -2,7 +2,21 @@
 
 namespace Rackspace.Threading
 {
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This class is used as the task type for instances of <see cref="TaskCompletionSource{TResult}"/> intended to
+    /// represent a <see cref="Task"/> which is not a <see cref="Task{TResult}"/>. Since this type is not externally
+    /// visible, users will not be able to cast the task to a <see cref="Task{TResult}"/>.
+    /// </summary>
+    /// <threadsafety static="true" instance="false"/>
     internal sealed class VoidResult
     {
+        /// <summary>
+        /// Prevents a default instance of the <see cref="VoidResult"/> class from being created.
+        /// </summary>
+        private VoidResult()
+        {
+        }
     }
 }
