d9d658014aab8736c3296985e691b54cfb05ee2c
Sam Harwell <sam@tunnelvisionlabs.com>
2015-07-01 17:38:21 -0500

    Add several reference types used for supporting async/await

---
+++ Rackspace.Threading/System/Runtime/CompilerServices/AsyncStateMachineAttribute.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+#if NET40PLUS
+
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(AsyncStateMachineAttribute))]
+
+#else
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Indicates whether a method is marked with the <see langword="async"/> modifier.
+    /// </summary>
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class AsyncStateMachineAttribute : StateMachineAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncStateMachineAttribute"/> class.
+        /// </summary>
+        /// <param name="stateMachineType">
+        /// The type object for the underlying state machine type that's used to implement a state machine method.
+        /// </param>
+        public AsyncStateMachineAttribute(Type stateMachineType)
+            : base(stateMachineType)
+        {
+        }
+    }
+}
+
+#endif
---
+++ Rackspace.Threading/System/Runtime/CompilerServices/IAsyncStateMachine.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+#if NET40PLUS
+
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(IAsyncStateMachine))]
+
+#else
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Represents state machines generated for asynchronous methods. This type is intended for compiler use only.
+    /// </summary>
+    public interface IAsyncStateMachine
+    {
+        /// <summary>Moves the state machine to its next state.</summary>
+        void MoveNext();
+
+        /// <summary>Configures the state machine with a heap-allocated replica.</summary>
+        /// <param name="stateMachine">The heap-allocated replica.</param>
+        void SetStateMachine(IAsyncStateMachine stateMachine);
+    }
+}
+
+#endif
