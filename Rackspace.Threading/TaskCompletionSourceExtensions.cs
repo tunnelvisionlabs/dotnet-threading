2397603da8f90c04c27b4958ebb8309b37f1b933
Sam Harwell <sam@tunnelvisionlabs.com>
2015-10-12 09:51:40 -0500

    Fix all violations of SA1028 (CodeMustNotContainTrailingWhitespace)

--- Rackspace.Threading/TaskCompletionSourceExtensions.cs
+++ Rackspace.Threading/TaskCompletionSourceExtensions.cs
@@ -1,7 +1,7 @@
 ﻿//--------------------------------------------------------------------------
-// 
-//  Copyright (c) Microsoft Corporation.  All rights reserved. 
-// 
+//
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//
 //  File: TaskCompletionSourceExtensions.cs
 //
 //--------------------------------------------------------------------------
