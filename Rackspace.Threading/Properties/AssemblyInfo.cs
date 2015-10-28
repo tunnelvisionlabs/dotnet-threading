84ffefe447a3495b94c5f9e637129317aebb9d44
Sam Harwell <sam@tunnelvisionlabs.com>
2015-10-28 00:23:03 -0500

    Prepare for next development iteration

--- Rackspace.Threading/Properties/AssemblyInfo.cs
+++ Rackspace.Threading/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 #endif
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyInformationalVersion("2.0.1-dev")]
